4500a824002aa587042f4ed4e21054eba1958b94
jocoonopa <jocoonopa@gmail.com>
2016-05-10 10:33:01 +0800

    Add Company address filter

--- app/Http/Controllers/Flap/CCS_OrderDivIndex/FindDivController.php
+++ app/Http/Controllers/Flap/CCS_OrderDivIndex/FindDivController.php
@@ -43,19 +43,11 @@ class FindDivController extends Controller
         }   
 
         $q = Processor::table('CCS_OrderDivIndex WITH(NOLOCK)')
-            ->select(DB::raw("MAX(CCS_OrderIndex.OrderNo) AS 單號,MAX(CCS_OrderIndex.KeyInDate) AS 出貨日,MAX(CCS_OrderIndex.OrderDate) AS 訂單日,MAX(CCS_OrderDivIndex.IndexSerNo) AS 流水號,MAX(CCS_OrderIndex.MustPayTotal) AS 應付帳款, MAX(POS_Member.Name) AS 會員姓名,COUNT(*) AS 分寄單數, MAX(CCS_OrderIndex.VerifyDate) AS 覆核日, MAX(CCS_OrderIndex.Status) AS 狀態"))
+            ->select(DB::raw("MAX(CCS_OrderIndex.OrderNo) AS 單號,MAX(CCS_OrderIndex.KeyInDate) AS 出貨日,MAX(CCS_OrderIndex.OrderDate) AS 訂單日,MAX(CCS_OrderDivIndex.IndexSerNo) AS 流水號,MAX(CCS_OrderIndex.MustPayTotal) AS 應付帳款, MAX(POS_Member.Name) AS 會員姓名,COUNT(*) AS 分寄單數, MAX(CCS_OrderIndex.VerifyDate) AS 覆核日, MAX(CCS_OrderIndex.Status) AS 狀態, MAX(CCS_OrderDivIndex.City) AS 縣市, MAX(CCS_OrderDivIndex.Town) AS 區, MAX(CCS_OrderDivIndex.Address) AS 地址"))
             ->leftJoin('CCS_OrderIndex WITH(NOLOCK)', 'CCS_OrderIndex.SerNo', '=', 'CCS_OrderDivIndex.IndexSerNo')
             ->leftJoin('POS_Member WITH(NOLOCK)', 'POS_Member.SerNo', '=', 'CCS_OrderIndex.MemberSerNo')
             ->groupBy('CCS_OrderDivIndex.IndexSerNo')
-            ->having('COUNT(*)', '>', 1)
-            // ->orWhere(function ($query) {
-            //     $query
-            //         ->having('COUNT(*)', '=', 1)
-            //         ->where('CCS_OrderDivIndex.town', '=', '新店區')
-            //         ->where('CCS_OrderDivIndex.city', '=', '新北市')
-            //         ->where('CCS_OrderDivIndex.address', 'like', '寶強路6%')
-            //     ;
-            // })
+            //->having('COUNT(*)', '>', 1)            
             ->orderBy('MAX(CCS_OrderIndex.KeyInDate)')
         ;
 
@@ -75,8 +67,29 @@ class FindDivController extends Controller
             $q->where('CCS_OrderIndex.KeyInDate', '<=', $this->endKeyInDate);
         }
 
-        dd(Processor::toSql($q));
+        //dd(Processor::toSql($q));
 
-        return Processor::getArrayResult($q);
+        return array_values(array_filter(Processor::getArrayResult($q), [$this, 'filter']));
+    }
+
+    protected function filter($var)
+    {
+        if (1 < $var['分寄單數']) {
+            return true;
+        }
+
+        $address = trim(keepOnlyChineseWord($var['地址']));
+
+        //pr($var);
+
+        if (1 === (int) $var['分寄單數'] 
+            && '新北市' === trim(keepOnlyChineseWord($var['縣市'])) 
+            && '新店區' === trim(keepOnlyChineseWord($var['區'])) 
+            && false !== strpos('寶強路', $address)
+        ) {
+            return true;
+        } 
+
+        return false;
     }
 }
