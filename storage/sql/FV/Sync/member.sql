2ac9813f8335484b485536267c26b4569dd55d3d
jocoonopa <jocoonopa@gmail.com>
2016-07-07 14:38:13 +0800

    Remove the condition about flags in the edit sqls

--- storage/sql/FV/Sync/member.sql
+++ storage/sql/FV/Sync/member.sql
@@ -82,8 +82,7 @@ SELECT * FROM (
         LEFT JOIN HRS_Employee WITH(NOLOCK)             ON HRS_Employee.SerNo = CCS_CRMFields.ExploitSerNoStr
         LEFT JOIN FAS_Corp                              ON FAS_Corp.SerNo = HRS_Employee.CorpSerNo 
         LEFT JOIN CCS_ShoppingBehaviorBrief WITH(NOLOCK) ON POS_Member.SerNo = CCS_ShoppingBehaviorBrief.MemberSerNoStr
-    WHERE POS_Member.LastModifiedDate >= '$mdtTime' 
-        OR CCS_MemberFlags.MDT_TIME >= '$mdtTime' 
+    WHERE POS_Member.LastModifiedDate >= '$mdtTime'
         OR CCS_ShoppingBehaviorBrief.MDT_TIME >= '$mdtTime'
         OR CCS_CRMFields.MDT_TIME >= '$mdtTime'
 ) AS Members WHERE Members.lineNum > $begin AND Members.lineNum <= $end 
