ea2eeb785f1372d9c89491bf2bcccb2fab63f452
jocoonopa <jocoonopa@gmail.com>
2016-07-22 14:53:29 +0800

    Sync/OrderExportHandler implement method

--- app/Export/FV/Sync/OrderExportHandler.php
+++ app/Export/FV/Sync/OrderExportHandler.php
@@ -7,7 +7,19 @@ class OrderExportHandler extends FVSyncExportHandler
     /**
      * @override
      */
-    protected function importFile($export){}
+    protected function importFile($export) 
+    {
+        $output = [];
+        
+        return exec('"C:\Program Files (x86)\Pivotal\Relation\Relation.exe" /d ' . env('VIG_SYS') . ' /agent CHOrderSync ' . basename($export->getInfo()['file']), $output, $status);
+    }
 
-    protected function genExportFilePath($export){}
+    protected function genExportFilePath($export)
+    {
+        if (!file_exists(env('FVSYNC_ORDER_STORAGE_PATH'))) {
+            mkdir(env('FVSYNC_ORDER_STORAGE_PATH'), 0777, true);
+        }
+        
+        return env('FVSYNC_ORDER_STORAGE_PATH') . $export->getType() . 'sync_export_' . time() . '.csv';
+    }
 }
