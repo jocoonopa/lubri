e3da9461e1c34f73e969d5bec68b2356dda477bd
jocoonopa <jocoonopa@gmail.com>
2016-07-29 14:26:59 +0800

    fix bonus log sql error

--- storage/sql/FV/Sync/member.sql
+++ storage/sql/FV/Sync/member.sql
@@ -82,10 +82,9 @@ SELECT * FROM (
         LEFT JOIN HRS_Employee WITH(NOLOCK)             ON HRS_Employee.SerNo = CCS_CRMFields.ExploitSerNoStr
         LEFT JOIN FAS_Corp WITH(NOLOCK)                 ON FAS_Corp.SerNo = HRS_Employee.CorpSerNo 
         LEFT JOIN CCS_ShoppingBehaviorBrief WITH(NOLOCK) ON POS_Member.SerNo = CCS_ShoppingBehaviorBrief.MemberSerNoStr
-        LEFT JOIN DCS_BonusLog WITH(NOLOCK)             ON POS_Member.SerNo = DCS_BonusLog.MemberSerNoStr
+        LEFT JOIN (SELECT MemberSerNoStr AS SerNo FROM DCS_BonusLog WITH(NOLOCK) WHERE Create_at >= '$mdtTime' GROUP BY MemberSerNoStr) AS _Bonus ON POS_Member.SerNo = _Bonus.SerNo
     WHERE POS_Member.LastModifiedDate >= '$mdtTime'
         OR CCS_ShoppingBehaviorBrief.MDT_TIME >= '$mdtTime'
         OR CCS_CRMFields.MDT_TIME >= '$mdtTime'
-        OR DCS_BonusLog.Create_at >= '$mdtTime'
 ) AS Members WHERE Members.lineNum > $begin AND Members.lineNum <= $end 
 ORDER BY Members.LastModifiedDate ASC
