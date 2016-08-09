149e75c3db4a7d98f0372e93974bc4275ec5dabc
jocoonopa <jocoonopa@gmail.com>
2016-08-09 23:36:35 +0800

    Implement CalllogExportHandler

--- app/Export/FV/Sync/CalllogExportHandler.php
+++ app/Export/FV/Sync/CalllogExportHandler.php
@@ -7,7 +7,19 @@ class CalllogExportHandler extends FVSyncExportHandler
     /**
      * @override
      */
-    protected function importFile($export){}
+    protected function importFile($export)
+    {
+    	$output = [];
+        
+        return exec('"C:\Program Files (x86)\Pivotal\Relation\Relation.exe" /d ' . env('VIG_SYS') . ' /agent CHCallLogSync ' . basename($export->getInfo()['file']), $output, $status);
+    }
 
-    protected function genExportFilePath($export){}
+    protected function genExportFilePath($export)
+    {
+    	 if (!file_exists(env('FVSYNC_CALLLOG_STORAGE_PATH'))) {
+            mkdir(env('FVSYNC_CALLLOG_STORAGE_PATH'), 0777, true);
+        }
+        
+        return env('FVSYNC_CALLLOG_STORAGE_PATH') . $export->getType() . 'sync_export_' . time() . '.csv';
+    }
 }
