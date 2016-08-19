d0a14e789e1594ffa7890bd369fb4a4daf74c614
jocoonopa <jocoonopa@gmail.com>
2016-08-19 15:32:23 +0800

    Trace date null give null

--- app/Export/Mould/FVListMould.php
+++ app/Export/Mould/FVListMould.php
@@ -4,14 +4,22 @@ namespace App\Export\Mould;
 
 class FVListMould extends FVMould
 {
-    protected $dateFields = ['活動開始時間', '活動結束時間', '追蹤日期', '撥打時間', '名單分配日期', '最後通話時間', '最後更改時間', '建立時間'];
+	const TRACE_DATE_COLUMN = '追蹤日期';
+
+    protected $dateFields = ['活動開始時間', '活動結束時間', self::TRACE_DATE_COLUMN, '撥打時間', '名單分配日期', '最後通話時間', '最後更改時間', '建立時間'];
 
     public function getRow(array $list)
     {
+    	$traceDate = array_get($dateFields,  self::TRACE_DATE_COLUMN);
+        
         $this->removeIgnoreColumn($list)->convertDateVal($list);
 
         foreach ($list as $colName => $val) {
             $list[$colName] = $this->transfer(array_get($list, $colName));
+        }
+
+        if (empty($traceDate)) {
+        	$list[ self::TRACE_DATE_COLUMN] = '';
         }
 
         return $list;
